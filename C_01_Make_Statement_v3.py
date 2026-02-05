574dcbb0143b99e6004f2d62847bd9db0a477be7
jhan202 <khanj3022@masseyhigh.school.nz>
2026-02-05 15:07:57 +1300

    Add files via upload

---
+++ C_01_Make_Statement_v3.py
@@ -0,0 +1,27 @@
+# Functions go here
+
+def make_statement(statement, decoration, lines=1):
+    """Creates headings(3 lines), subheadings (2 lines) and emphasised
+    text / mini-headings (1 line). Only use emojis for single line
+    statements"""
+
+
+
+    middle = f"{decoration * 3} {statement} {decoration * 3}"
+    top_bottom = decoration * len(middle)
+
+    if lines == 1:
+        print(middle)
+    elif lines == 2:
+        print(middle)
+        print(top_bottom)
+
+    else:
+        print(top_bottom)
+        print(middle)
+        print(top_bottom)
+
+# Main Routine goes here
+make_statement("Programming is Fun!","=", 3)
+make_statement("Programming is Still Fun!","*", 2)
+make_statement("Emoji in Action", "🎶")
